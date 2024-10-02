0c600e408c82b436e89a43cf13396a0dd96f0e20
euhloloh <eloisa.miranda@escola.pr.gov.br>
2024-10-02 16:51:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+  background ('white"')
+}
+function draw() {
+
+  stroke("blue");
+  fill("red");
+  if (mouseIsPressed);
+  rect(mouseX, mouseY, 35, 35);
+}
